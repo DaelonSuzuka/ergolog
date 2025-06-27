77463a8781f269b889ae81109354851533542c4d
DaelonSuzuka <daelonsuzuka@gmail.com>
2025-06-26 23:27:06 -0400

    add more tests

--- test/test_basic.py
+++ test/test_basic.py
@@ -44,26 +44,64 @@ def test_child_loggers(caplog: LogCaptureFixture):
     assert caplog.records[0].name == 'ergo'
     assert caplog.records[1].name == 'ergo.one'
     assert caplog.records[2].name == 'ergo.one.two'
-    
+
+
 def test_tags(caplog: LogCaptureFixture):
     with eg.tag('a'):
         eg.info('one tag')
         with eg.tag('b'):
             eg.info('two tags')
         eg.info('one tag')
-            
+
     assert len(caplog.records) == 3
-    
-    assert caplog.records[0].tags == '[a] '
-    assert caplog.records[1].tags == '[a, b] '
-    assert caplog.records[2].tags == '[a] '
-    
-    
+
+    assert caplog.records[0].tags == '[a] '  # type: ignore
+    assert caplog.records[1].tags == '[a, b] '  # type: ignore
+    assert caplog.records[2].tags == '[a] '  # type: ignore
+
+
 def test_kwtags(caplog: LogCaptureFixture):
-    with eg.tag(keyword='tags', comma='multiple'):
+    with eg.tag(a='a'):
         eg.debug('')
-        with eg.tag('regular_tag'):
+        with eg.tag('tag'):
             eg.info('')
-            with eg.tag(more='keywords'):
+            with eg.tag(b='b'):
                 eg.info('')
         eg.debug('')
+
+    assert len(caplog.records) == 4
+
+    assert caplog.records[0].tags == '[a=a] '  # type: ignore
+    assert caplog.records[1].tags == '[a=a, tag] '  # type: ignore
+    assert caplog.records[2].tags == '[a=a, tag, b=b] '  # type: ignore
+    assert caplog.records[3].tags == '[a=a] '  # type: ignore
+
+
+def test_tag_decorator(caplog: LogCaptureFixture):
+    @eg.tag('inner')
+    def inner():
+        eg.info('test')
+
+    @eg.tag('outer')
+    def outer():
+        eg.debug('before')
+        inner()
+
+        eg.debug('after')
+
+    eg.debug('start')
+    outer()
+    eg.debug('end')
+
+    assert len(caplog.records) == 5
+
+    assert caplog.records[0].tags == ''  # type: ignore
+    assert caplog.records[0].message == 'start'
+    assert caplog.records[1].tags == '[outer] '  # type: ignore
+    assert caplog.records[1].message == 'before'
+    assert caplog.records[2].tags == '[outer, inner] '  # type: ignore
+    assert caplog.records[2].message == 'test'
+    assert caplog.records[3].tags == '[outer] '  # type: ignore
+    assert caplog.records[3].message == 'after'
+    assert caplog.records[4].tags == ''  # type: ignore
+    assert caplog.records[4].message == 'end'
